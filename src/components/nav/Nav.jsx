d64addb446fa5af2c1ba2916cdc7b6f0f7f6e370
Pedro.sandrini <pedro.sandri@markdigx.com.br>
2022-05-10 12:01:53 -0300

    [Pedro] escolhendo icons para barra de navegação

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {RiBook2Line} from 'react-icons/ri'
+import {FaRegHandshake} from 'react-icons/fa'
+import {AiOutlineMessage} from 'react-icons/ai'
+
 export const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser /></a>
+      <a href="#experience"><RiBook2Line /></a>
+      <a href="#services"><FaRegHandshake /></a>
+      <a href="#contact"><AiOutlineMessage /></a>
+    </nav>
   )
 }
